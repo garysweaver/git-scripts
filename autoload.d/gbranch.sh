de689a9416d8ddbdc47a42c8cf640c2a8a307b0c
Gary S. Weaver <garysweaver@gmail.com>
2010-11-24 12:42:28 -0500

    gbranch fix: sometimes would branch off of current branch rather than master

--- autoload.d/gbranch.sh
+++ autoload.d/gbranch.sh
@@ -2,6 +2,12 @@
 #
 # Written by Gary S. Weaver
 #
+# Release: 2010-11-24
+# gbranch (branch_name) without [optional_remote_branch_to_branch_from]
+# would sometimes unintentionally branch from the current branch. To
+# fix, a call to gbranch (branch_name) is now the same as:
+# gbranch (branch_name) master
+#
 # Release: 2010-06-28
 # added gbranch (branch_name) [optional_remote_branch_to_branch_from]
 # which tracks a remote branch if it already exists, or branches from
@@ -16,40 +22,18 @@
 
 function gbranch () {
   branch=$1
-  optionalbranchfrom=$2
+  branchfrom=$2
   if test $# -eq 1
   then
-    rbrch=`git branch -r`
-    if [ "$?" -eq "0" ]
-    then
-      if [[ "$rbrch" =~ "origin/$branch" ]]
-      then
-        git checkout -b "$branch" --track "origin/$branch"
-        if [ "$?" -ne "0" ]
-        then
-          git checkout "$branch"
-        fi
-      else
-        git checkout -b "$branch"
-        if [ "$?" -eq "0" ]
-        then
-          # The following lines track the remote branch per: http://djwonk.com/blog/2009/04/18/tracking-remote-git-branches/
-          # note: first two lines help avoid  "fatal: Cannot force update the current branch." error per DJWonk.
-          git push origin "$branch"
-          git checkout master
-          git branch -f "$branch" "origin/$branch"
-          git checkout "$branch"
-        else
-          git checkout "$branch"
-        fi
-      fi
-    fi
-  elif test $# -eq 2
+    branchfrom="master"
+  fi
+
+  if test $# -gt 0 && test $# -lt 3
   then
     rbrch=`git branch -r`
     if [ "$?" -eq "0" ]
     then
-      if [[ "$rbrch" =~ "origin/$optionalbranchfrom" ]] 
+      if [[ "$rbrch" =~ "origin/$branchfrom" ]] 
       then
         if [[ "$rbrch" =~ "origin/$branch" ]]
         then
@@ -60,14 +44,14 @@ function gbranch () {
             git checkout "$branch"
           fi
         else
-          git co --no-track -b "$branch" "origin/$optionalbranchfrom"
+          git co --no-track -b "$branch" "origin/$branchfrom"
           git push origin "$branch"
           # this only works in git 1.7.0 and above
           git branch --set-upstream "$branch" "origin/$branch"
           if [ "$?" -ne "0" ]
           then
             echo "It looks like either you aren't using git 1.7.0 or above, or something went wrong."
-            echo "To add tracking of the new branch to the specified remote branch, edit .git/config"
+            echo "To add tracking of the new branch to the corresponding remote branch, edit .git/config"
             echo "and add the following:"
             echo
             echo "[branch \"$branch\"]"
@@ -76,7 +60,7 @@ function gbranch () {
           fi
         fi
       else
-        echo "'origin/$optionalbranchfrom' did not exist remotely, so did not attempt to branch from it."
+        echo "'origin/$branchfrom' did not exist remotely, so did not attempt to branch from it."
         echo
         echo "usage: gbranch (branch_name) [optional_remote_branch_name_to_branch_from_if_not_master]"
       fi
